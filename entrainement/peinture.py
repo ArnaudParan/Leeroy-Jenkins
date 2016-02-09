1f37b7a3bbf17911c9e740e227c4d6b95df44bd4
Nerzadler <lcressot@gmail.com>
2016-02-09 14:07:21 +0100

    0 1

--- entrainement/peinture.py
+++ entrainement/peinture.py
@@ -10,7 +10,21 @@ import os, sys
 # Maths with Numpy
 import numpy as np
 
+# convertie en np 0 1
+def format_numpy(nr, nc, data):
+	res = np.zeros([nr,nc])
+	for i in range(0,nr):
+		for j in range(0,nc):
+			if data[i][j] == '#':
+				res[i][j] = 1
+	return res
+
+
+# retourne une liste de tuple (densite, size)
+def calcul_densite(nr, nc, data):
+	
 
 
 def process_data_method1(nr, nc, data):
-	print data
+	for i in range(nc):
+		print data[i,:]
