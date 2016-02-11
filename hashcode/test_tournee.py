cfdd8bb212bdd664335793f9b1b3118b3d2f87dc
arnaud_paran <paran.arnaud@gmail.com>
2016-02-11 22:18:35 +0100

    avance sur tournee

--- hashcode/test_tournee.py
+++ hashcode/test_tournee.py
@@ -11,20 +11,42 @@ class client:
         self.c = c
         self.demands = inventory
 
+class drone:
+    def __init__(self, r, c, package):
+        self.r = r
+        self.c = c
+        self.package = package
+
 class tournee_testCase(unittest.TestCase):
     def test_nearest_neighbor(self):
         client_list =  [client(0, 10, []), client(0, 5, [])]
         self.assertEqual(1, nearest_neighbor(0, 0, client_list))
 
     def test_available_clients(self):
-        client_inv1 = {0: 1, 1: 2 , 2: 4}
-        client_inv2 = {0: 3, 1: 2 , 3: 1}
+        client_inv1 = {0: -1, 1: -2 , 2: -4}
+        client_inv2 = {0: -3, 1: -2 , 3: -1}
         client1 = client(0, 0, client_inv1)
         client2 = client(0, 0, client_inv2)
         client_list = [client1, client2]
         inventory = {3: 4}
         current = available_clients(inventory, client_list)
         self.assertEqual([client2], current)
+
+    def test_item_to_deliver(self):
+        client_inv1 = {0: -1, 1: -2 , 2: -1}
+        client1 = client(0, 0, client_inv1)
+        inventory = {2: 4}
+        current = item_to_deliver(inventory, client1)
+        self.assertEqual((2, 1), current)
+
+    def test_incr_drone_time(self):
+        client1 = client(0, 10, {})
+        client2 = client(1, 2, {})
+        client_list =  [client1, client2]
+        t_arr = [1, 2]
+        curr_drone = drone(0, 0, {})
+        incr_drone_time(curr_drone, client2, t_arr, 1)
+        self.assertEqual([1, 6], t_arr)
 
 if __name__ == "__main__":
     unittest.main()
--- hashcode/tournee.py
+++ hashcode/tournee.py
@@ -1,8 +1,19 @@
 # -*- coding: utf-8 -*-
 
+import math
+
 #Arnaud
 
 #we have to define an array with the clients which will be used only there
+
+#warning, the client_list is custom
+def tournees(drones, client_list, initial_time):
+    dronesTimes = initial_time
+    #select the min time drone
+    #take his nearest neighbor ##
+    #take the item to deliver, and the number ##
+    #deliver the item ##
+    #add time
 
 def nearest_neighbor(initR, initC, client_list):
     currClientId = 0
@@ -21,10 +32,32 @@ def available_clients(inventory, client_list):
     for client in client_list:
         for itemId in client.demands:
             if itemId in inventory:
-                if inventory[itemId] > 0 and client.demands[itemId] > 0:
+                if inventory[itemId] > 0 and client.demands[itemId] < 0:
                     av_clients.append(client)
                     break
     return av_clients
 
+def item_to_deliver(inventory, client):
+    for itemId in client.demands:
+        if itemId in inventory:
+            nbInv = inventory[itemId]
+            nbDem = client.demands[itemId]
+            if nbInv > 0 and nbDem < 0:
+                return itemId, min(nbInv, -nbDem)
+
+def deliver_item(drone, client, itemId, nbItem):
+    client.demands[clientId] = client.demands[clientId]  + nbItem
+    drone.package[clientId] = drone.package[clientId]  - nbItem
+
+def movement_time(r1, c1, r2, c2):
+    sq_dist = square_distance(r1, c1, r2, c2)
+    dist = math.sqrt(sq_dist)
+    return math.ceil(dist)
+
+def incr_drone_time(drone, client, times_array, droneId):
+    mov_time = movement_time(drone.r, drone.c, client.r, client.c) + 1
+    times_array[droneId] = times_array[droneId] + mov_time
+
 def square_distance(r1, c1, r2, c2):
     return ((r1 - r2)**2) + ((c1 - c2)**2)
+
