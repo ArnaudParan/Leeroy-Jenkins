025b61b20bd688b32d7233ef770de308deb646d0
Simon Lebastard <lebastardsimon@gmail.com>
2016-02-11 21:51:44 +0100

    Class drone, version 1

---
+++ hashcode/fichier1.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 11 21:15:49 2016
+
+@author: Simon
+"""
+
+def weight(package):
+    pack_weight = 0
+    for key in package:
+        pack_weight += products[key].weight
+    return pack_weight
+        
+
+###### CLASSE DRONE ######
+
+class Drone:
+    def __init__(self,r,c,package_list):
+        self.r = r
+        self.c = c
+        self.package = to_dict(package_list)
+        self.cost = 0
+        self.wht = weight(self.package)
+        
+    def move(self, to_r, to_c):
+        self.cost += int(square_distance(self.r, self.c, to_r, to_c)) + 1
+        self.r = to_r
+        self.c = to_c
+        
+    def carriable(self, order):
+        is_deliverable = True
+        after_delivery = add_dict(package, order)
+        for key in after_delivery:
+            if after_delivery[key] < 0:
+                is_deliverable = False
+        return is_deliverable
+        
+    def take(self, order):
+        if(self.deliverable(order)):
+            add_dict(self.package, order)
+            
+        
+            
+        
+        
