7f7ca6734c20837d2e72eead2b7a5bc356a055cf
Simon Lebastard <lebastardsimon@gmail.com>
2016-02-11 21:57:20 +0100

    fichier1 update

--- hashcode/fichier1.py
+++ hashcode/fichier1.py
@@ -10,17 +10,19 @@ def weight(package):
     for key in package:
         pack_weight += products[key].weight
     return pack_weight
-        
+ WHT_LIMIT = 100       
 
 ###### CLASSE DRONE ######
 
 class Drone:
-    def __init__(self,r,c,package_list):
+    def __init__(self,drone_id,r,c,package_list):
+        self.id = drone.id
         self.r = r
         self.c = c
         self.package = to_dict(package_list)
         self.cost = 0
         self.wht = weight(self.package)
+        self.wht_limit = WHT_LIMIT
         
     def move(self, to_r, to_c):
         self.cost += int(square_distance(self.r, self.c, to_r, to_c)) + 1
@@ -31,13 +33,19 @@ class Drone:
         is_deliverable = True
         after_delivery = add_dict(package, order)
         for key in after_delivery:
-            if after_delivery[key] < 0:
+            if after_delivery[key] < 0 and self.wht + weight(order) <= self.wht_limit:
                 is_deliverable = False
         return is_deliverable
         
     def take(self, order):
         if(self.deliverable(order)):
             add_dict(self.package, order)
+            self.wht += weight(order)
+            
+class Product:
+    def __init__(self, prod_id, wht):
+        self.id = prod_id
+        self.wht = wht
             
         
             
