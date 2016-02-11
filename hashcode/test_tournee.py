c066bcc9b177088d9e5e87281d8a304fe7beefde
arnaud_paran <paran.arnaud@gmail.com>
2016-02-11 21:33:07 +0100

    ajout de la sélection des clients livrables

--- hashcode/test_tournee.py
+++ hashcode/test_tournee.py
@@ -6,14 +6,25 @@ from tournee import *
 import unittest
 
 class client:
-    def __init__(self, r, c):
+    def __init__(self, r, c, inventory):
         self.r = r
         self.c = c
+        self.demands = inventory
 
 class tournee_testCase(unittest.TestCase):
     def test_nearest_neighbor(self):
-        client_list =  [client(0, 10), client(0, 5)]
+        client_list =  [client(0, 10, []), client(0, 5, [])]
         self.assertEqual(1, nearest_neighbor(0, 0, client_list))
+
+    def test_available_clients(self):
+        client_inv1 = {0: 1, 1: 2 , 2: 4}
+        client_inv2 = {0: 3, 1: 2 , 3: 1}
+        client1 = client(0, 0, client_inv1)
+        client2 = client(0, 0, client_inv2)
+        client_list = [client1, client2]
+        inventory = {3: 4}
+        current = available_clients(inventory, client_list)
+        self.assertEqual([client2], current)
 
 if __name__ == "__main__":
     unittest.main()
--- hashcode/tournee.py
+++ hashcode/tournee.py
@@ -16,5 +16,15 @@ def nearest_neighbor(initR, initC, client_list):
             currMinDist = dist
     return currClientId
 
+def available_clients(inventory, client_list):
+    av_clients = []
+    for client in client_list:
+        for itemId in client.demands:
+            if itemId in inventory:
+                if inventory[itemId] > 0 and client.demands[itemId] > 0:
+                    av_clients.append(client)
+                    break
+    return av_clients
+
 def square_distance(r1, c1, r2, c2):
     return ((r1 - r2)**2) + ((c1 - c2)**2)
