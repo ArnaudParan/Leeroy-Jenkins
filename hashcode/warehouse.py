4743b111fdeaffa4ba2ff9304d3491920bebfe17
VictorSanh <s.rotciv@hotmail.fr>
2016-02-11 21:09:19 +0100

    Update warehouse.py

--- hashcode/warehouse.py
+++ hashcode/warehouse.py
@@ -2,7 +2,15 @@
 
 class WareHouse:
 	#listeProduit, si il y a deux produits de types 100 : [100,100]
+	#self.produits est un dictionnaire.
 	def __init__(self, listeCoord, listeProduit):
-		self.coord = listeCoord
-		self.produits = listeProduit
+		self.r = listeCoord[0]
+		self.x = listeCoord[1]
+		self.produits = {}
+		for produitID in listeProduit:
+			if produitID in self.produits.keys():
+				self.produits[produitID] += 1
+			else:
+				self.produits[produitID] = 1
+			
 	
